9c2b2f53f71ea52a9acfa4754fc40fb623d603df
Anna Burley <anna.burley3@gmail.com>
2019-01-03 23:16:03 +0000

    Added onDismiss method

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ class App extends Component {
     this.state = {
       list
     };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
   }
 
   render() {
